e6fbe2dc833b0c877264673b9608bff915ebdd93
zairax7u7 <126507734+zairax7u7@users.noreply.github.com>
2023-03-04 18:42:02 -0600

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//ejercicios con todos
+let number1 = "mamili";
+
+
+let nom = prompt ("¿cual es tu nombre?")
+let nom1 = alert(`Mucho gusto ${nom} a mi sistema`)
+let procedure = prompt("Escribe la contraseña para ver el mensaje")
+
+if (procedure == number1){
+    alert("Te ganste un bibi mi panoshita chiquita y gudita")
+}else {
+    alert (`Pero que mensa ${nom} jajaajajaj recarga la pagina para poder reintentarlo`)
+} 
